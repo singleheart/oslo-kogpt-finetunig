9eca898115f7353d670c47efd2920b5e55f03568
Jay <jinn.kim@sk.com>
2022-08-11 21:44:06 +0900

    fix install order

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/tunib-ai/oslo.git
-git checkout 85a4ff11816f8319a7344f1e596dd6b3e7592034
 
 cd oslo
+git checkout 85a4ff11816f8319a7344f1e596dd6b3e7592034
 pip install --editable .
